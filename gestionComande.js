408c7b78ba914b1b036490cbfa63a868538e8d92
mariem2012 <mariemdianibafa00@gmail.com>
2024-09-05 11:30:11 +0000

    Modify all file

--- gestionComande.js
+++ gestionComande.js
@@ -2,35 +2,90 @@ export class Customer {
   constructor() {}
 
   createCustomer(customer) {
-    this.id = customer.id;
     this.name = customer.name;
     this.email = customer.email;
-    console.log(
-      `Customer: ${this.id}, ${this.name}, ${this.email} has been created`
-    );
-  }
-
-  loadDataFromDb(newCustomer) {
-    this.id = newCustomer.id;
-    this.name = newCustomer.name;
-    this.email = newCustomer.email;
+    console.log(`Customer: ${this.name}, ${this.email} has been created`);
   }
 
   destroyCustomer() {
-    console.log(
-      `Customer: ${this.id}, ${this.name}, ${this.email} has been deleted`
-    );
+    console.log(`Customer: ${this.name}, ${this.email} has been deleted`);
   }
 
   editCustomer(newCustomer) {
-    this.id = newCustomer.id;
     this.name = newCustomer.name;
     this.email = newCustomer.email;
-    console.log(
-      `Customer: ${this.id}, ${this.name}, ${this.email} has been updated`
-    );
+    console.log(`Customer: ${this.name}, ${this.email} has been updated`);
   }
   getCustomer() {
-    console.log(`Customer: ${this.id}, ${this.name}, ${this.email}`);
+    console.log(`Customer: ${this.name}, ${this.email}`);
+    return { name: this.name };
+  }
+}
+
+export class Product {
+  constructor() {}
+
+  createProduct(product) {
+    this.name = product.name;
+    this.quantity = product.quantity;
+    this.price = product.price;
+    console.log(`Product: ${this.name} has been created`);
+  }
+
+  destroyProduct() {
+    console.log(`Product: ${this.name} has been deleted`);
+  }
+
+  editProduct(newProduct) {
+    this.name = newProduct.name;
+    this.quantity = newProduct.quantity;
+    this.price = newProduct.price;
+    console.log(`Product: ${this.name} has been updated`);
+  }
+  getProduct() {
+    console.log(`Product: ${this.name}`);
+    return { name: this.name, quantity: this.quantity, price: this.price };
+  }
+}
+
+export class OrderDetail {
+  constructor(quantity, price, product) {
+    this.quantity = quantity;
+    this.price = price;
+    this.product = product;
+  }
+}
+
+export class Order {
+  constructor() {}
+
+  createOrder(order) {
+    this.date = order.date;
+    this.amount = order.amount;
+    this.details = order.details;
+    this.customer = order.customer;
+    console.log(`Order has been created`);
+  }
+
+  destroyOrder() {
+    this.date = null;
+    console.log(`Order has been deleted`);
+  }
+
+  editOrder(newOrder) {
+    this.date = newOrder.date;
+    this.amount = newOrder.amount;
+    this.details = newOrder.details;
+    this.customer = newOrder.customer;
+    console.log(`Order has been updated`);
+  }
+
+  getOrder() {
+    return {
+      date: this.date,
+      amount: this.amount,
+      details: this.details.length,
+      customer: this.customer.name,
+    };
   }
 }
--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import express from "express";
 import bodyParser from "body-parser";
-import { Customer } from "./gestionComande.js";
+import { Customer, Order, OrderDetail, Product } from "./gestionComande.js";
 
 const app = express();
 const port = 3080;
@@ -8,22 +8,78 @@ app.use(bodyParser.json());
 app.get("/", (req, res) => {
   res.send("Bonjour les simploniens");
 });
-app.post("/customers", (req, res) => {
+
+app.post("/app", (req, res) => {
   const customer = new Customer();
-  customer.createCustomer({ id: 1, name: "Fatima", email: "fatima@gmail.com" });
-  res.send(`Welcome ${customer.name}`);
+  customer.createCustomer({ name: "Fatima", email: "fatima@gmail.com" });
+
+  const p1 = new Product();
+  p1.createProduct({ name: "Dress", quantity: 12, price: 200 });
+
+  const p2 = new Product();
+  p2.createProduct({ name: "Stylo", quantity: 4, price: 5 });
+
+  const p3 = new Product();
+  p3.createProduct({ name: "Mac", quantity: 5, price: 1000 });
+
+  const d1 = new OrderDetail({ quantity: 2, prix: p1.price, product: p1 });
+  const d2 = new OrderDetail({ quantity: 1, prix: p2.price, product: p2 });
+  const d3 = new OrderDetail({ quantity: 2, prix: p3.price, product: p3 });
+
+  let result = [];
+
+  /** Create order */
+  const order = new Order();
+  order.createOrder({
+    date: new Date(),
+    amount: 2405,
+    customer: customer.getCustomer(),
+    details: [d1, d2, d3],
+  });
+
+  result.push(order.getOrder()); // Adding first order in result array
+
+  customer.editCustomer({ name: "Mohamed", email: "med@gmail.com" }); //Update curstomer data
+
+  //Update order data
+  order.editOrder({
+    date: new Date(),
+    amount: 2405,
+    customer: customer.getCustomer(),
+    details: [d1, d2, d3],
+  });
+
+  /***** Association des elements simple */
+  const association = {
+    customer: customer.getCustomer(),
+    p1: p1.getProduct(),
+    p2: p2.getProduct(),
+  };
+  result.push(order.getOrder());
+
+  const items = order.details;
+  const p4 = new Product();
+  p4.createProduct({ name: "Phone", quantity: 12, price: 200 });
+  items.push(new OrderDetail({ quantity: 1, price: p4.price, product: p4 }));
+
+  //Update order data
+  order.editOrder({
+    date: new Date(),
+    amount: 2405,
+    customer: customer.getCustomer(),
+    details: items,
+  });
+
+  let status = 200;
+  let message = order;
+  if (!order) {
+    message = "Not found order";
+    status = 400;
+  }
+
+  res.status(status).json({ order, result, association });
 });
 
-app.put("/customers/:id", (req, res) => {
-  // declaration du modele
-  const customer = new Customer();
-  customer.loadDataFromDb({ id: 5, name: "Mariame", email: "mari@gmail.com" });
-  console.log(`old id is ${customer.id}`);
-
-  customer.editCustomer({ id: 10, name: "Harouna", email: "hr@gmail.com" });
-  console.log(`current id is ${customer.id}`);
-  res.send(`Welcome ${customer.name}`);
-});
 app.listen(port, () => {
   console.log(`L'application est en ecoute sur port ${port}`);
 });
